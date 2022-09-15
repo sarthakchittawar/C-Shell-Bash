9e637dfff0ff79f8c8f7fa6544a53f9a0811a8b2
Sarthak Chittawar <sarthakchitt@gmail.com>
2022-09-15 16:52:56 +0530

    io redirection

--- process.c
+++ process.c
@@ -26,6 +26,9 @@
 
 int process(char *process_name, int andflag, char *dir, char *curr_dir, char *init_dir, char *prev_dir, char *username, int *timeflag, char *prompt, char **hist, int histno, int *bgcount, int bg_processes[], double *bef, double *en)
 {
+    char prname[MAX];
+    strcpy(prname, process_name);
+
     char *token = strtok(process_name, "\t ");
     char **arr = (char **)malloc(MAX * __SIZEOF_POINTER__);
     int c = 0;
@@ -34,6 +37,106 @@ int process(char *process_name, int andflag, char *dir, char *curr_dir, char *in
         arr[c] = token;
         c++;
         token = strtok(NULL, "\t ");
+    }
+    int inflag = 0, outflag = 0, in_backup, out_backup, in_index = -1, out_index = -1;
+    FILE *fp;
+    for (int i = 0; i < c; i++)
+    {
+        if (i != c - 1 && strcmp(arr[i], "<") == 0)
+        {
+            inflag = 1;
+            in_backup = dup(STDIN_FILENO);
+            char path[MAX];
+            if ((fp = fopen(arr[i + 1], "r")) != NULL)
+            {
+                dup2(fileno(fp), STDIN_FILENO);
+                in_index = i;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (i != c - 1 && strcmp(arr[i], ">") == 0)
+        {
+            outflag = 1;
+            dup2(STDOUT_FILENO, out_backup);
+            if ((fp = fopen(arr[i + 1], "w")) != NULL)
+            {
+                dup2(fileno(fp), STDOUT_FILENO);
+                out_index = i;
+            }
+            else
+            {
+                if (open(arr[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644) < 0)
+                {
+                    perror("Failed to open file");
+                    return 0;
+                }
+                fp = fopen(arr[i + 1], "r");
+
+                dup2(fileno(fp), STDOUT_FILENO);
+                out_index = i;
+            }
+        }
+        else if (i != c - 1 && strcmp(arr[i], ">>") == 0)
+        {
+            outflag = 2;
+            dup2(STDOUT_FILENO, out_backup);
+            if ((fp = fopen(arr[i + 1], "a")) != NULL)
+            {
+                dup2(fileno(fp), STDOUT_FILENO);
+                out_index = i;
+            }
+            else
+            {
+                if (open(arr[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644) < 0)
+                {
+                    perror("Failed to open file");
+                    return 0;
+                }
+                fp = fopen(arr[i + 1], "a");
+
+                dup2(fileno(fp), STDOUT_FILENO);
+                out_index = i;
+            }
+        }
+    }
+
+    char *tok = strtok(prname, "\t ");
+    c = 0;
+    while (tok != NULL)
+    {
+        if (inflag == 0 && outflag == 0)
+        {
+            arr[c] = tok;
+            c++;
+        }
+        else if (inflag == 0 && outflag != 0)
+        {
+            if (c != out_index && c != out_index + 1)
+            {
+                arr[c] = tok;
+                c++;
+            }
+        }
+        else if (inflag != 0 && outflag == 0)
+        {
+            if (c != in_index && c != in_index + 1)
+            {
+                arr[c] = tok;
+                c++;
+            }
+        }
+        else if (inflag != 0 && outflag != 0)
+        {
+            if (c != out_index && c != out_index + 1 && c != in_index && c != in_index + 1)
+            {
+                arr[c] = tok;
+                c++;
+            }
+        }
+        tok = strtok(NULL, "\t ");
     }
 
     // to reset bgcount
@@ -62,12 +165,18 @@ int process(char *process_name, int andflag, char *dir, char *curr_dir, char *in
                     }
                 }
             }
+            fclose(proc);
         }
         if (flag == 0 && strcmp(arr[0], "pinfo") != 0) // exception
         {
             (*bgcount) = 0;
         }
     }
+
+    // if (inflag != 0 && outflag != 0)
+    // {
+    //     dup2(STDOUT_FILENO, STDIN_FILENO);
+    // }
 
     if (strcmp(arr[0], "pwd") == 0)
     {
@@ -86,50 +195,55 @@ int process(char *process_name, int andflag, char *dir, char *curr_dir, char *in
         if (cd(process_name, dir, curr_dir, init_dir, prev_dir, username, arr, c) == 0)
         {
             free(arr);
+            dup2(in_backup, STDIN_FILENO);
+            dup2(out_backup, STDOUT_FILENO);
             return 0;
         }
         free(arr);
-        return 1;
     }
     else if (strcmp(arr[0], "ls") == 0)
     {
         if (ls_main(process_name, dir, curr_dir, init_dir, prev_dir, username, arr, c) == 0)
         {
             free(arr);
+            dup2(in_backup, STDIN_FILENO);
+            dup2(out_backup, STDOUT_FILENO);
             return 0;
         }
         free(arr);
-        return 1;
     }
     else if (strcmp(arr[0], "history") == 0)
     {
         if (history(c, hist, histno) == 0)
         {
             free(arr);
+            dup2(in_backup, STDIN_FILENO);
+            dup2(out_backup, STDOUT_FILENO);
             return 0;
         }
         free(arr);
-        return 1;
     }
     else if (strcmp(arr[0], "pinfo") == 0)
     {
         if (pinfo(c, arr, curr_dir, bg_processes, *bgcount) == 0)
         {
             free(arr);
+            dup2(in_backup, STDIN_FILENO);
+            dup2(out_backup, STDOUT_FILENO);
             return 0;
         }
         free(arr);
-        return 1;
     }
     else if (strcmp(arr[0], "discover") == 0)
     {
         if (discover(c, arr, curr_dir, init_dir) == 0)
         {
             free(arr);
+            dup2(in_backup, STDIN_FILENO);
+            dup2(out_backup, STDOUT_FILENO);
             return 0;
         }
         free(arr);
-        return 1;
     }
     else if (strcmp(arr[0], "exit") == 0)
     {
@@ -141,4 +255,8 @@ int process(char *process_name, int andflag, char *dir, char *curr_dir, char *in
         others(arr, c, process_name, andflag, dir, curr_dir, init_dir, prev_dir, username, timeflag, prompt, hist, histno, bgcount, bg_processes, bef, en);
         free(arr);
     }
+    dup2(in_backup, STDIN_FILENO);
+    dup2(out_backup, STDOUT_FILENO);
+
+    return 1;
 }
